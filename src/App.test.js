96484ddde32ac0e958f49f6c64a81d2ebc696fd2
Apiwat  Panitram <47608559+ImGotsila@users.noreply.github.com>
2025-06-12 19:10:42 +0700

    Improve navbar brand test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders navbar brand text', () => {
   render(<App />);
-  const brandElement = screen.getByText(/ระบบจัดการสต็อก/);
+  const brandElement = screen.getByText('ระบบจัดการสต็อก', { exact: false });
   expect(brandElement).toBeInTheDocument();
 });
