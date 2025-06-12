dd24f9107e6e9c2fd1bfbc97899c239aa5601bd2
Apiwat  Panitram <47608559+ImGotsila@users.noreply.github.com>
2025-06-12 19:01:10 +0700

    Add missing dependencies and update tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Polyfill ResizeObserver for components relying on it (e.g. Recharts)
+class ResizeObserver {
+  constructor(callback) {
+    this.callback = callback;
+  }
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+global.ResizeObserver = global.ResizeObserver || ResizeObserver;
