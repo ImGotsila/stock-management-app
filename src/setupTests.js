0097ee66a168324a2747e8871f6ac10c01b481da
Apiwat  Panitram <47608559+ImGotsila@users.noreply.github.com>
2025-06-12 19:01:40 +0700

    Update tests for navbar brand

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Polyfill ResizeObserver for components that rely on it (e.g., recharts)
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+window.ResizeObserver = window.ResizeObserver || ResizeObserver;
